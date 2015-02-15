d43a7d7c38d4e05a3359dc02586009de75caa258
Mikhail Stolovich <stolovich@yahoo.com>
2015-02-15 20:52:38 +0200

    fixes of tm

---
+++ Game/GestureGame/Assets/Scripts/MouseFollow.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MouseFollow : MonoBehaviour
+{
+
+    public float distance = 10;
+
+    // Update is called once per frame
+    void Update()
+    {
+        Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+        Vector3 pos = ray.GetPoint(distance);
+        transform.position = pos;
+	}
+}
